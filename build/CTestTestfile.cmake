f47e96d77edba42576e2a5e680d8f695836eccb5
mchamrami <mchamrami@users.noreply.github.com>
2025-11-17 10:26:12 +0330

    Remove build directory from repo and ignore it

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/Desktop/embedded_devops/embedded-week1-starter
-# Build directory: D:/Desktop/embedded_devops/embedded-week1-starter/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
